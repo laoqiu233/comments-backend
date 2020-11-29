7364e61f5d923fef2cf3166dfdbb8c5e2e6ada2e
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2020-11-29 19:28:46 +1000

    init

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.path.join(os.getcwd(), 'data.db'))
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    published = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+@app.route('/api/comments', methods=['GET', 'POST'])
+def comments_handler():
+    if (request.method == 'GET'):
+        resp = []
+        for comment in Comment.query.all():
+            resp.append({
+                'User': comment.username,
+                'Body': comment.body,
+                'Published': int(comment.published.timestamp())
+            })
+        return jsonify(resp)
+    elif (request.method == 'POST'):
+        if (not request.values.get('username', '') or not request.values.get('body', '')):
+            return jsonify({'msg': 'Bad Request'}), 400
+        else:
+            new_comment = Comment(
+                username=request.values['username'], 
+                body=request.values['body']
+            )
+            db.session.add(new_comment)
+            db.session.commit()
+            return jsonify({
+                'User': new_comment.username,
+                'Body': new_comment.body,
+                'Published': int(new_comment.published.timestamp())
+            }), 201
+
+if (__name__ == '__main__'):
+    db.create_all()
+    app.run(
+        host='0.0.0.0',
+        port=8080,
+        debug=(os.environ.get('PY_ENV', '') != 'Production' )
+    )
