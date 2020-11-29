2d628af74ee77dd8f6552f9b7383069d8b27af5f
Dmitri Tsiu <dmitrimeowsalot@gmail.com>
2020-11-29 19:36:39 +1000

    Changed port

--- app.py
+++ app.py
@@ -44,6 +44,6 @@ if (__name__ == '__main__'):
     db.create_all()
     app.run(
         host='0.0.0.0',
-        port=8080,
+        port=80,
         debug=(os.environ.get('PY_ENV', '') != 'Production' )
     )
